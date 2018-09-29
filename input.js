663b344af772f8294381da52e153570fba342731
Lee Presswood <LeePresswood@gmail.com>
2018-09-29 13:04:44 -0500

    Created index and input outline.

---
+++ input.js
@@ -0,0 +1,10 @@
+/**
+ * List of inputs spread across an array.
+ *
+ * Normalize the inputs to be between 0 and 1.
+ * Use -1 for null, undefined, unknown, or don't care.
+ *
+ */
+module.exports = [
+
+];
