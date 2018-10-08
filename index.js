61abd6890cad1d64d59cd15db24612bee09b0283
Lee Presswood <LeePresswood@gmail.com>
2018-10-07 22:36:50 -0500

    Documented and normalized the metadata.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const Perceptron = require("synaptic").Architect.Perceptron;
 const MidiConvert = require("midiconvert");
 const Song = require("./music/Song");
@@ -6,13 +7,11 @@ const OUTPUT_SIZE = require("./nn/trainingData").OUTPUT_SIZE;
 const getInputOutputs = require("./nn/trainingData").getInputOutputs;
 
 //Load song into MIDI.
-MidiConvert.load("./assets/beethoven_opus10_1.mid")
-    .then((midiFile) => {
-
-    });
+const midiBlob = fs.readFileSync("./assets/beethoven_opus10_1.mid", "binary");
+const midi = MidiConvert.parse(midiBlob);
 
 //Load MIDI into parsable metadata/note windows.
-const song = new Song();
+const song = new Song(midi);
 
 //Get the set of training data
 const traningData = getInputOutputs(song);
--- music/Song.js
+++ music/Song.js
@@ -8,15 +8,38 @@ module.exports = class Song {
      * @param {object} data MIDI data for the entire song.
      */
     constructor(data) {
-        this.metadata = new Metadata(4, 4);
+        /*
+        "data" looks something like this:
+        {
+          header:
+           { PPQ: 480, timeSignature: [ 3, 4 ], bpm: 183.31973711949698 },
+          tracks:
+           [ t {
+               name: 'Klaviersonate No. 5 -  1. Satz',
+               channelNumber: 0,
+               notes: [Array],
+               controlChanges: [Object],
+               instrumentNumber: 0,
+               id: 0 } ] }
+        */
+        const header = data.header;
+        const tracks = data.tracks;
+        this.metadata = new Metadata(header.PPQ, header.timeSignature, header.bpm);
         this.windows = [];
     }
 };
 
 class Metadata {
-    constructor(timeSignatureNumerator, timeSignatureDenominator) {
-        this.timeSignatureNumerator = timeSignatureNumerator;
-        this.timeSignatureDenominator = timeSignatureDenominator;
+    constructor(ppq, [timeSignatureNumerator, timeSignatureDenominator], bpm) {
+        //PPQ -- Pulses per quarter-note. Generally much lower than 1000. Normalized out of 1000.
+        this.ppq = Math.min(ppq, 1000) / 1000;
+
+        //Time signature numerator and denominator. Normalized out of 20.
+        this.timeSignatureNumerator = Math.min(timeSignatureNumerator, 20) / 20
+        this.timeSignatureDenominator = Math.min(timeSignatureDenominator, 20) / 20;
+
+        //BPM -- Beats per minute. Normalized out of 1000.
+        this.bpm = Math.min(bpm, 1000) / 1000;
     }
 }
 
